1392d09e069243741c890d76b2b78be10df3e616
Bartek_Pawlik <g00400535@atu.ie>
2025-04-07 15:09:07 +0000

    API key changes

--- src/app/weather-api.service.ts
+++ src/app/weather-api.service.ts
@@ -5,22 +5,25 @@ import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
+
+const SECRET_KEY : any = "581fd81ff5d1d33bb28ecb1145a5733a";
+
 export class WeatherAPIService {
 
   constructor(private httpClient : HttpClient) { }
 
   // function gets weather data for the city a user enters
   getWeatherDataByCity(lat: number, lon : number): Observable<any> {
-    return this.httpClient.get(`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=581fd81ff5d1d33bb28ecb1145a5733a&units=metric`);
+    return this.httpClient.get(`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=${SECRET_KEY}&units=metric`);
   }
 
   getCityNameFromCoords(lat: number, lon: number): Observable<any> {
-    const url = `https://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&limit=1&appid=581fd81ff5d1d33bb28ecb1145a5733a`;
+    const url = `https://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&limit=1&appid=${SECRET_KEY}`;
     return this.httpClient.get(url);
   }
   
   getCoordsFromCityName(cityName: string): Observable<any> {
-    const url = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=581fd81ff5d1d33bb28ecb1145a5733a`;
+    const url = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${SECRET_KEY}`;
     return this.httpClient.get(url);
   }
 }
